5f73d870a394b5ad621fefdf2e61c7a726a71adf
HustleCoding <f.dobinciuc7@gmail.com>
2022-01-06 11:10:26 +0200

    [BookablesList component added]

--- src/components/Bookables/BookablesPage.js
+++ src/components/Bookables/BookablesPage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import BookablesList from "./BookablesList";
 
 const BookablesPage = () => {
   return (
     <main className="bookables-page">
-      <p>Bookables</p>
+      <BookablesList />
     </main>
   );
 };
